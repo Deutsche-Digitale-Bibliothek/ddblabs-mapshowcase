9fd6b1bd8460337f6b52de42585e656230341d9e
Lukas Baron, in medias res GmbH <baron@webgis.de>
2014-04-22 16:06:18 +0200

    Bump release to v0.1.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ requires = [
     ]
 
 setup(name='ddb',
-      version='0.1',
+      version='0.1.1',
       description='ddb map showcase',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
